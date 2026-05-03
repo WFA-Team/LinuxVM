a5d3bc3fe799d8d0bce15d797cd93a147aa53ed7
koveed.arora <koveeda@gmail.com>
2026-05-03 23:41:18 +0530

    install kcat on wsl distro by default for debugging purposes

--- docker_setup_wsl/prerequisites.sh
+++ docker_setup_wsl/prerequisites.sh
@@ -5,6 +5,7 @@ dos2unix install_docker.sh
 dos2unix kafka_container.sh
 dos2unix docker-compose.yml
 dos2unix ksql_schema.sh
-bash ./install_docker.sh
+/bin/bash ./install_docker.sh
 sg docker -c './kafka_container.sh'
 sg docker -c './ksql_schema.sh'
+apt install kcat
